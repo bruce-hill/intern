d4f174e90165746b37114cb57503c7bab2a6c516
Bruce Hill <bruce@bruce-hill.com>
2022-09-05 00:01:57 -0400

    Move pragma to top of file

--- intern.h
+++ intern.h
@@ -1,8 +1,8 @@
+#pragma once
 // Simple API for interning values.
 // Interned values are garbage collected using the Boehm garbage collector (not
 // immortal), but the most recently interned items are kept in memory to
 // prevent churn.
-#pragma once
 
 // Intern a chunk of bytes which may or may not contain references to GC objects
 const void *intern_bytes(const char *bytes, size_t len);
